63a98b6c32d54abc8c0326e0897ca243b5c6350a
Deniz Oguz <denizoguz@gmail.com>
2019-05-14 10:08:08 +0300

    reformat

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import {Editor, EditorContext, WithEditorActions} from "@atlaskit/editor-core";
+// import {Editor, EditorContext, WithEditorActions} from "@atlaskit/editor-core";
+import Editor from "@atlaskit/editor-core/editor"
+import EditorContext from "@atlaskit/editor-core/ui/EditorContext"
+import WithEditorActions from "@atlaskit/editor-core/ui/WithEditorActions"
 import React from "react";
 
 function Test() {
